7d9b67709aef3afe7add5a22b6f0938e90048ea9
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2022-08-21 22:12:31 +0900

    use the `react-textarea-autosize` component

--- src/components/BorderSwitchableTextarea.tsx
+++ src/components/BorderSwitchableTextarea.tsx
@@ -1,17 +1,22 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
+import ReactTextareaAutosize from "react-textarea-autosize";
 
 export const BorderSwitchableTextarea = () => {
   const [text, setText] = useState("");
+
   const className = !text
     ? "textarea textarea-bordered resize-y w-full sm:w-[26rem] h-[64px] text-2xl text-neutral text-center bg-transparent border-dashed border-2 focus:textarea-accent my-8"
     : "textarea border-none resize-y w-[26rem] text-5xl text-neutral text-center bg-transparent focus:textarea-accent my-8";
 
+  const onChange = (e: ChangeEvent<HTMLTextAreaElement>) =>
+    setText(e.target.value);
+
   return (
-    <textarea
-      rows={1}
+    <ReactTextareaAutosize
       className={className}
+      style={{ resize: "none" }}
       placeholder="Type here"
-      onChange={(e) => setText(e.target.value)}
+      onChange={onChange}
       data-testid="non-border"
     />
   );
