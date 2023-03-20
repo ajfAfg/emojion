4110e22d4db5e24016d792d8cccd70488d35b75c
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2023-03-21 03:39:25 +0900

    mock `useRouter` not working on node

--- __tests__/components/BorderSwitchableTextarea.test.tsx
+++ __tests__/components/BorderSwitchableTextarea.test.tsx
@@ -2,6 +2,18 @@ import { BorderSwitchableTextarea } from "@/components/BorderSwitchableTextarea"
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
+// NOTE: C.f. https://github.com/vercel/next.js/issues/7479#issuecomment-533657701
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      route: "/",
+      pathname: "",
+      query: "",
+      asPath: "",
+    };
+  },
+}));
+
 describe("BorderSwitchableTextarea", () => {
   it("has non-border style when typed", async () => {
     const { getByTestId } = render(<BorderSwitchableTextarea />);
