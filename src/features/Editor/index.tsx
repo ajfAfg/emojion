16f8acbb51ac23556826c323c40c3827652361f0
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2023-04-06 03:00:10 +0900

    remove the button posting an image to Twitter

--- src/features/Editor/index.tsx
+++ src/features/Editor/index.tsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import domtoimage from "dom-to-image";
 import { FC } from "react";
 import { Canvas } from "./components/Canvas";
@@ -22,34 +21,6 @@ export const Editor: FC = () => {
       });
   };
 
-  // TODO: Move logic into another layer (e.g. use-case)
-  const openTwitterWebIntentDeprecated = async () => {
-    const blobToBase64 = async (blob: Blob) => {
-      return new Promise((resolve, _) => {
-        const reader = new FileReader();
-        reader.onloadend = () => resolve(reader.result);
-        reader.readAsDataURL(blob);
-      }).then((encoded) => (encoded as string).split(",")[1]); // NOTE: Remove 'data:image/png;base64,'
-    };
-
-    const blob = await domtoimage.toBlob(
-      document.getElementById("capture") as Node
-    );
-    const encodedImage = await blobToBase64(blob);
-
-    const { imageUrl } = await axios({
-      method: "POST",
-      url: "/api/uploadImage",
-      data: { encodedImage },
-    }).then(({ data }) => data);
-
-    window.open(
-      `https://twitter.com/intent/tweet?url=${imageUrl}`,
-      "_blank",
-      "height=570,width=520"
-    );
-  };
-
   const openTwitterWebIntent = async () => {
     window.open(
       `https://twitter.com/intent/tweet?url=${encodeURIComponent(
@@ -70,15 +41,6 @@ export const Editor: FC = () => {
         <button className="btn btn-tweet" onClick={openTwitterWebIntent}>
           Tweet
         </button>
-
-        <div
-          className="tooltip"
-          data-tip="Scheduled to be discontinued as a result of the Twitter API fee"
-        >
-          <button className="btn" onClick={openTwitterWebIntentDeprecated}>
-            Tweet
-          </button>
-        </div>
 
         <button className="btn btn-primary" onClick={exportImage}>
           Export
